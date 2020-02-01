733cdb45352ffcea3fbead483504fa24658b84ff
federueda <55557961+federueda@users.noreply.github.com>
2020-02-01 03:47:09 +0100

    Update Producer_A.py

--- src/Producer_A.py
+++ src/Producer_A.py
@@ -3,7 +3,7 @@ import time
 
 producera = KafkaProducer(bootstrap_servers='localhost:9092')
 i=0
-while i<20:
+while i<40:
     string_a="Producer_A_"+str(i)
     key_a = string_a.encode()	#tobytes
     producera.send('sample',key=key_a, value=bytes(i))
