14df0085de1ea8b94280c6f09cc1ad07d99d8c8f
federueda <55557961+federueda@users.noreply.github.com>
2020-02-01 03:41:55 +0100

    Update Producer_B.py

--- src/Producer_B.py
+++ src/Producer_B.py
@@ -3,7 +3,9 @@ import time
 
 producerb = KafkaProducer(bootstrap_servers='localhost:9092')
 i=1
-while i<10:
-    producera.send('sample',key=b'Number: ', value=bytes(i))
+while i<20:
+    string_b="Producer_B_"+str(i)
+    key_b = string_b.encode()	#tobytes
+    producerb.send('sample',key=key_b, value=bytes(i))
     time.sleep(2)
     i+=2
