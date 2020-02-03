287b4fc95bcbbd3ceda499a4ffdd45ceb9661756
FR <55557961+federueda@users.noreply.github.com>
2020-02-03 17:25:52 +0100

    Update Producer_A.py

--- src/Producer_A.py
+++ src/Producer_A.py
@@ -1,11 +1,12 @@
 from kafka import KafkaProducer
 import time
 
-producera = KafkaProducer(bootstrap_servers='localhost:9092')
-i=0
-while i<40:
-    string_a="Producer_A_"+str(i)
-    key_a = string_a.encode()	#tobytes
-    producera.send('sample',key=key_a, value=bytes(i))
+producer_a = KafkaProducer(bootstrap_servers='localhost:9092')
+i = 0
+
+while i < 50:
+    string = "Producer A -> Number "+str(i)
+    message_a = string.encode("utf-8", errors="replace")    # change to bytes
+    producer_a.send('sample', message_a)
     time.sleep(1)
-    i+=2
+    i += 2
